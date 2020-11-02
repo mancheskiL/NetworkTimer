3f2bb218c4aa329ea6e3a87175a3d2977665f16f
Lucas <lucas.mancheskicol@gmail.com>
2020-11-02 22:03:31 +0100

    starting network timer project

---
+++ vue/App.vue
@@ -0,0 +1,14 @@
+<template>
+    <HelloWorld msg="Hello World from Vue!"/>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+    name: 'App',
+    components: {
+        HelloWorld
+    }
+}
+</script>
---
+++ vue/main-vue.js
@@ -0,0 +1,5 @@
+const vue = require('vue')
+// import { createApp } from 'vue'
+import App from './App.vue'
+
+vue.createApp(App).mount('#app')
