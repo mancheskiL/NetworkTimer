efa123f9eda46aee64473b789ac437fe04b02e6f
Lucas <lucas.mancheskicol@gmail.com>
2020-11-04 22:01:26 +0100

    making/styling input route

--- network-timer/src/App.vue
+++ network-timer/src/App.vue
@@ -1,15 +1,14 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <Container class="container"/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Container from './components/Container.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Container
   }
 }
 </script>
@@ -22,5 +21,13 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+.container {
+  border-style: solid;
+  padding: 10px;
+  width: 300px;
+  height: 400px;
+  margin: auto;
 }
 </style>
---
+++ network-timer/src/components/Container.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="container">
+        <input id='input'>
+        <button v-on:click="submit">Submit</button>
+        <button v-on:click="clear">Clear</button>
+    </div>
+</template>
+
+<script>
+// import Input from './Input.vue'
+
+export default {
+    name: 'Container',
+    methods: {
+        submit() {
+            // pass
+        },
+        clear() {
+            // pass
+        }
+    }
+}
+</script>
+
+<style scoped>
+    button {
+        padding: 10px
+    }
+</style>
