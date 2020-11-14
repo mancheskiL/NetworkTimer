c38cfa92c3a2cf7b174d1e0984aa112a0c6ffe93
Lucas <lucas.mancheskicol@gmail.com>
2020-11-14 12:19:37 +0100

    new icon and title

--- network-timer/src/background.js
+++ network-timer/src/background.js
@@ -3,6 +3,7 @@
 import { app, protocol, BrowserWindow, ipcMain } from 'electron'
 import { createProtocol } from 'vue-cli-plugin-electron-builder/lib'
 import installExtension, { VUEJS_DEVTOOLS } from 'electron-devtools-installer'
+import path from 'path'
 const isDevelopment = process.env.NODE_ENV !== 'production'
 
 // Scheme must be registered before the app is ready
@@ -19,7 +20,9 @@ async function createWindow() {
       // Use pluginOptions.nodeIntegration, leave this alone
       // See nklayman.github.io/vue-cli-plugin-electron-builder/guide/security.html#node-integration for more info
       nodeIntegration: process.env.ELECTRON_NODE_INTEGRATION
-    }
+    },
+    icon: path.join(__static, 'icon.png'),
+    title: "Network Timer"
   })
   win.removeMenu()
 
@@ -83,7 +86,9 @@ ipcMain.on('new-window', (event, packet) => {
   const win = new BrowserWindow(
     {
       width: 400,
-      height: 300
+      height: 300,
+      icon: path.join(__static, 'icon.png'),
+      title: "Network Timer"
     }
   )
   // win.loadURL('http://192.168.178.43:3000')
